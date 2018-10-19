6cab8f35120e757ca8ff6144fb45e0fa72cc2414
jaebooker <jaesonbooker@gmail.com>
2018-10-19 14:41:42 -0700

    added some to readme

--- ReadMe.md
+++ ReadMe.md
@@ -5,13 +5,13 @@ A decentralized dAPP run on a local Ethereum test blockchain network, using Soli
 
 ## Installation
 
-1. Install Truffle globally by running 'npm install -g truffle' in the console.
-2. Install and run the `liteserver` development server (outside the development console).
-3. Install Ganache (https://truffleframework.com/ganache), then make sure its port is set to 8545.
-4. Run 'Truffle Migrate --Reset' in the terminal.
+1. Install Truffle globally by running 'npm install -g truffle' in the terminal.
+2. Run "truffle develop" in the terminal.
+4. Run 'migrate --reset' in the developer console.
 5. Add Metamask add-on to Chrome or use Brave with Metamask enabled.
-6. Set Metamask to equal the local server running on Ganache.
-7. run 'npm run dev' in the terminal.
-8. Page should load automatically. If not, it can be located at http://localhost:3000.
-
+6. Set Metamask to equal the local server running on truffle develop (it should have printed to terminal when you started running it)
+7. In a terminal separate from the Truffle Developer one, run 'npm run start'
+8. Page should load automatically. If not, it can be located at http://localhost:3000
+9. Click "Sign Up" and fill in your name. Metamask should open a window requesting "gas". Click "submit" in the Metamask window. This should bring you to a welcome page, with a list of projects to join. If not, you can view them in "/projects" (if you are logged in).
+10. You can run the tests by running "truffle test" in the normal terminal.
 Thanks to Truffle boxes and their tutorials for their assistance
--- src/layouts/projects/Projects.js
+++ src/layouts/projects/Projects.js
@@ -16,56 +16,56 @@ class Projects extends Component {
   </div>
   <div class="panel-body">
   // <img alt="140x140" data-src="holder.js/140x140" class="img-rounded img-center" src="://amp.businessinsider.com/images/5b9235bb5c5e5254548b59f5-1136-852.jpg" data-holder-rendered="true"></img>
-  <strong>Project Type</strong>: <span class="project-type">Making the Internet Great Again</span><br/>
-  <strong>Date</strong>: <span class="project-date">102018</span><br/>
+  <strong>Project Type</strong>: <span class="project-type">We are utilizing blockchain technology to Make The Internet Great Again</span><br/>
+  <strong>Date</strong>: <span class="project-date">10/19/2018</span><br/>
   <strong>Location</strong>: <span class="project-location">Remote</span><br/><br/>
   <button class="btn btn-default btn-join" type="button" data-id="0">Join</button>
   </div>
   </div>
   <div class="panel panel-default panel-project">
   <div class="panel-heading">
-    <h3 class="panel-title">Make the Internet Great Again</h3>
+    <h3 class="panel-title">Fly Catching App</h3>
   </div>
   <div class="panel-body">
   // <img alt="140x140" data-src="holder.js/140x140" class="img-rounded img-center" src="://amp.businessinsider.com/images/5b9235bb5c5e5254548b59f5-1136-852.jpg" data-holder-rendered="true"></img>
-  <strong>Project Type</strong>: <span class="project-type">Making the Internet Great Again</span><br/>
-  <strong>Date</strong>: <span class="project-date">102018</span><br/>
-  <strong>Location</strong>: <span class="project-location">Remote</span><br/><br/>
+  <strong>Project Type</strong>: <span class="project-type">A practical app for catching house flies</span><br/>
+  <strong>Date</strong>: <span class="project-date">10/01/2018</span><br/>
+  <strong>Location</strong>: <span class="project-location">San Francisco</span><br/><br/>
   <button class="btn btn-default btn-join" type="button" data-id="1">Join</button>
   </div>
   </div>
   <div class="panel panel-default panel-project">
   <div class="panel-heading">
-    <h3 class="panel-title">Make the Internet Great Again</h3>
+    <h3 class="panel-title">Le Elon Musk</h3>
   </div>
   <div class="panel-body">
   // <img alt="140x140" data-src="holder.js/140x140" class="img-rounded img-center" src="://amp.businessinsider.com/images/5b9235bb5c5e5254548b59f5-1136-852.jpg" data-holder-rendered="true"></img>
-  <strong>Project Type</strong>: <span class="project-type">Making the Internet Great Again</span><br/>
-  <strong>Date</strong>: <span class="project-date">102018</span><br/>
-  <strong>Location</strong>: <span class="project-location">Remote</span><br/><br/>
+  <strong>Project Type</strong>: <span class="project-type">Using perfum engineering to make a special cologne inspired by a certain rocket-building billionaire.</span><br/>
+  <strong>Date</strong>: <span class="project-date">10/10/2018</span><br/>
+  <strong>Location</strong>: <span class="project-location">Paris, France</span><br/><br/>
   <button class="btn btn-default btn-join" type="button" data-id="2">Join</button>
   </div>
   </div>
   <div class="panel panel-default panel-project">
   <div class="panel-heading">
-    <h3 class="panel-title">Make the Internet Great Again</h3>
+    <h3 class="panel-title">Aliiive</h3>
   </div>
   <div class="panel-body">
   // <img alt="140x140" data-src="holder.js/140x140" class="img-rounded img-center" src="://amp.businessinsider.com/images/5b9235bb5c5e5254548b59f5-1136-852.jpg" data-holder-rendered="true"></img>
-  <strong>Project Type</strong>: <span class="project-type">Making the Internet Great Again</span><br/>
-  <strong>Date</strong>: <span class="project-date">102018</span><br/>
+  <strong>Project Type</strong>: <span class="project-type">A simple survey to determine if you are, in fact, alive.</span><br/>
+  <strong>Date</strong>: <span class="project-date">10/20/2018</span><br/>
   <strong>Location</strong>: <span class="project-location">Remote</span><br/><br/>
   <button class="btn btn-default btn-join" type="button" data-id="3">Join</button>
   </div>
   </div>
   <div class="panel panel-default panel-project">
   <div class="panel-heading">
-    <h3 class="panel-title">Make the Internet Great Again</h3>
+    <h3 class="panel-title">2 Flies 2 Furious</h3>
   </div>
   <div class="panel-body">
   // <img alt="140x140" data-src="holder.js/140x140" class="img-rounded img-center" src="://amp.businessinsider.com/images/5b9235bb5c5e5254548b59f5-1136-852.jpg" data-holder-rendered="true"></img>
-  <strong>Project Type</strong>: <span class="project-type">Making the Internet Great Again</span><br/>
-  <strong>Date</strong>: <span class="project-date">102018</span><br/>
+  <strong>Project Type</strong>: <span class="project-type">When catching one fly just is not enough!</span><br/>
+  <strong>Date</strong>: <span class="project-date">10/31/2018</span><br/>
   <strong>Location</strong>: <span class="project-location">Remote</span><br/><br/>
   <button class="btn btn-default btn-join" type="button" data-id="4">Join</button>
   </div>
@@ -79,7 +79,7 @@ class Projects extends Component {
   <strong>Project Type</strong>: <span class="project-type">Making the Internet Great Again</span><br/>
   <strong>Date</strong>: <span class="project-date">102018</span><br/>
   <strong>Location</strong>: <span class="project-location">Remote</span><br/><br/>
-  <button class="btn btn-default btn-join" type="button" data-id="5">Join</button>
+  <button class="btn btn-default btn-join" type="button" var data-id-5="5">Join</button>
   </div>
   </div>
   </div>
